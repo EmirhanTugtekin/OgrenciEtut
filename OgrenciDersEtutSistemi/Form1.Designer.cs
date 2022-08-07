892c33caa56b0dc3859e0c0f235e0bdcbb27893b
EmirhanTugtekin <emirhann20@gmail.com>
2022-08-07 04:49:20 +0300

    first commit

---
+++ OgrenciDersEtutSistemi/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace OgrenciDersEtutSistemi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cmbOgretmen = new System.Windows.Forms.ComboBox();
+            this.btnOlustur = new System.Windows.Forms.Button();
+            this.mskSaat = new System.Windows.Forms.MaskedTextBox();
+            this.mskTarih = new System.Windows.Forms.MaskedTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbDers = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.btnEtutDetay = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cmbOgretmen);
+            this.groupBox1.Controls.Add(this.btnOlustur);
+            this.groupBox1.Controls.Add(this.mskSaat);
+            this.groupBox1.Controls.Add(this.mskTarih);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.cmbDers);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(592, 351);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // cmbOgretmen
+            // 
+            this.cmbOgretmen.FormattingEnabled = true;
+            this.cmbOgretmen.Location = new System.Drawing.Point(126, 100);
+            this.cmbOgretmen.Name = "cmbOgretmen";
+            this.cmbOgretmen.Size = new System.Drawing.Size(200, 32);
+            this.cmbOgretmen.TabIndex = 9;
+            // 
+            // btnOlustur
+            // 
+            this.btnOlustur.Location = new System.Drawing.Point(126, 299);
+            this.btnOlustur.Name = "btnOlustur";
+            this.btnOlustur.Size = new System.Drawing.Size(127, 33);
+            this.btnOlustur.TabIndex = 8;
+            this.btnOlustur.Text = "Etüt Oluştur";
+            this.btnOlustur.UseVisualStyleBackColor = true;
+            this.btnOlustur.Click += new System.EventHandler(this.btnOlustur_Click);
+            // 
+            // mskSaat
+            // 
+            this.mskSaat.Location = new System.Drawing.Point(126, 248);
+            this.mskSaat.Mask = "00:00";
+            this.mskSaat.Name = "mskSaat";
+            this.mskSaat.Size = new System.Drawing.Size(200, 29);
+            this.mskSaat.TabIndex = 7;
+            this.mskSaat.ValidatingType = typeof(System.DateTime);
+            // 
+            // mskTarih
+            // 
+            this.mskTarih.Location = new System.Drawing.Point(126, 198);
+            this.mskTarih.Mask = "00/00/0000";
+            this.mskTarih.Name = "mskTarih";
+            this.mskTarih.Size = new System.Drawing.Size(200, 29);
+            this.mskTarih.TabIndex = 6;
+            this.mskTarih.ValidatingType = typeof(System.DateTime);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 253);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 24);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Saat:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 203);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 24);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Tarih:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Öğretmen:";
+            // 
+            // cmbDers
+            // 
+            this.cmbDers.FormattingEnabled = true;
+            this.cmbDers.Location = new System.Drawing.Point(126, 47);
+            this.cmbDers.Name = "cmbDers";
+            this.cmbDers.Size = new System.Drawing.Size(200, 32);
+            this.cmbDers.TabIndex = 1;
+            this.cmbDers.SelectedIndexChanged += new System.EventHandler(this.cmbDers_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ders:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.checkBox1);
+            this.groupBox2.Controls.Add(this.btnEtutDetay);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Location = new System.Drawing.Point(631, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(452, 351);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(152, 187);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(62, 28);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "İptal";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // btnEtutDetay
+            // 
+            this.btnEtutDetay.Location = new System.Drawing.Point(148, 258);
+            this.btnEtutDetay.Name = "btnEtutDetay";
+            this.btnEtutDetay.Size = new System.Drawing.Size(127, 33);
+            this.btnEtutDetay.TabIndex = 8;
+            this.btnEtutDetay.Text = "Etüt Detay";
+            this.btnEtutDetay.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(46, 188);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 24);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Durum:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(50, 140);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(68, 24);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "Soyad:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(79, 87);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(39, 24);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Ad:";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.dataGridView1);
+            this.groupBox3.Location = new System.Drawing.Point(12, 384);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(1071, 351);
+            this.groupBox3.TabIndex = 9;
+            this.groupBox3.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 25);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1065, 323);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1095, 749);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnOlustur;
+        private System.Windows.Forms.MaskedTextBox mskSaat;
+        private System.Windows.Forms.MaskedTextBox mskTarih;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbDers;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button btnEtutDetay;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox cmbOgretmen;
+    }
+}
+
---
+++ OgrenciDersEtutSistemi/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace OgrenciDersEtutSistemi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection connection = new SqlConnection("Data Source=DESKTOP-6TUVH6H;Initial Catalog=EtutTest;Integrated Security=True");
+
+        void dersListesi()
+        {
+            
+            SqlDataAdapter sda = new SqlDataAdapter("select * from tbldersler", connection);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+
+            cmbDers.ValueMember = "DERSID";
+            cmbDers.DisplayMember = "DERSAD";
+            cmbDers.DataSource = dt;
+            
+        }
+
+        void etutListesi()
+        {
+            SqlDataAdapter sda = new SqlDataAdapter("execute etut", connection);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+
+            dataGridView1.DataSource = dt;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dersListesi();
+            etutListesi();
+        }
+
+        private void cmbDers_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            SqlDataAdapter sda=new SqlDataAdapter("select * from tblogretmen where BRANSID="+cmbDers.SelectedValue,connection);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+
+            cmbOgretmen.ValueMember = "OGRTID";
+            cmbOgretmen.DisplayMember = "AD";
+            cmbOgretmen.DataSource = dt;
+            
+        }
+
+        private void btnOlustur_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("insert into tbletut (DERSID,OGRETMENID,TARIH,SAAT) values (@p1,@p2,@p3,@p4)",connection);
+            command.Parameters.AddWithValue("@p1", cmbDers.SelectedValue);
+            command.Parameters.AddWithValue("@p2", cmbOgretmen.SelectedValue);
+            command.Parameters.AddWithValue("@p3", mskTarih.Text);
+            command.Parameters.AddWithValue("@p4", mskSaat.Text);
+
+            command.ExecuteNonQuery();
+            MessageBox.Show("etüt eklendi", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            etutListesi();
+            connection.Close();
+        }
+    }
+}
